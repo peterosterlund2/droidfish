f284180da8f5c732235c36e1252860e25545b40d
Peter Osterlund <peterosterlund2@gmail.com>
2015-05-23 13:35:12 +0200

    DroidFish: Define IS_64BIT when compiling stockfish for 64 bit architectures. About 2% faster on HTC One M9.

--- DroidFish/jni/stockfish/Android.mk
+++ DroidFish/jni/stockfish/Android.mk
@@ -5,15 +5,26 @@ SF_SRC_FILES := \
 	bitbase.cpp endgame.cpp material.cpp movepick.cpp position.cpp timeman.cpp ucioption.cpp \
 	bitboard.cpp evaluate.cpp misc.cpp search.cpp tt.cpp syzygy/tbprobe.cpp
 
+MY_ARCH_DEF :=
+ifeq ($(TARGET_ARCH_ABI),arm64-v8a)
+  MY_ARCH_DEF += -DIS_64BIT
+endif
+ifeq ($(TARGET_ARCH_ABI),x86_64)
+  MY_ARCH_DEF += -DIS_64BIT
+endif
+ifeq ($(TARGET_ARCH_ABI),mips64)
+  MY_ARCH_DEF += -DIS_64BIT
+endif
+
 include $(CLEAR_VARS)
 LOCAL_MODULE    := stockfish-nopie
 LOCAL_SRC_FILES := $(SF_SRC_FILES)
-LOCAL_CFLAGS    := -std=c++11 -O2
+LOCAL_CFLAGS    := -std=c++11 -O2 $(MY_ARCH_DEF)
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := stockfish
 LOCAL_SRC_FILES := $(SF_SRC_FILES)
-LOCAL_CFLAGS    := -std=c++11 -O2 -fPIE
+LOCAL_CFLAGS    := -std=c++11 -O2 -fPIE $(MY_ARCH_DEF)
 LOCAL_LDFLAGS	+= -fPIE -pie
 include $(BUILD_EXECUTABLE)
